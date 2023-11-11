dd8665533019923bd5b40414cec1ebd5e519428e
Alina Tibulca <tibulkalina@mail.ru>
2023-11-11 18:55:29 +0100

    added 100% coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class StatisticsServiceTest {
-
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
@@ -14,6 +13,54 @@ public class StatisticsServiceTest {
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxStartingWithLowest() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWithEqualIncomes() {
+        StatisticsService service = new StatisticsService();
+
+        long[] equalIncomes = {7, 7, 7, 7, 7, 7, 7};
+        long expected = 7;
+
+        long actual = service.findMax(equalIncomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWhenIncomesContainNegativeNumbers() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {-5, -10, -2, -8, -7};
+        long expected = -2;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWhenIncomesContainDuplicateMaxValues() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {10, 5, 8, 1, 7, 4, 10};
+        long expected = 10;
+
+        long actual = service.findMax(incomes);
 
         Assertions.assertEquals(expected, actual);
     }
